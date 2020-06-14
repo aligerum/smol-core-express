78a9d934394f4dcf2ffb74753e8190eda09ad829
Allister Aligerum <allister@aligerum.io>
2020-06-14 13:36:38 -0400

    add skeleton

---
+++ skeleton/data/routes.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  // add http routes
+  http(router) {
+
+    // example
+    // router.resource('/someroute', 'somecontroller')
+    // router.post('/someroute/someaction', 'somecontroller.somefunction')
+
+  },
+
+  // add socket routes
+  ws(socket) {
+  },
+
+}
