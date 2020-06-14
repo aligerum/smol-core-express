ef327a6d5391795bccb0469a62e8e4d9c1c683dd
Allister Aligerum <allister@aligerum.io>
2020-06-13 21:32:32 -0400

    add deploy and serve commands

---
+++ command/serve.js
@@ -0,0 +1,61 @@
+const execSync = require('child_process').execSync
+const fs = require('fs')
+const smol = require('smol')
+const smolConfig = smol.config()
+const coreConfig = smol.config(smol.coreName)
+
+module.exports = {
+  description: 'Serve on configured port',
+  args: [
+    'action?: Action to perform',
+  ],
+  argValues: {
+    action: [
+      'restart: Restart server background process',
+      'status: Get status of server',
+      'start: Start server as a background process',
+      'stop: Stop server background process',
+    ],
+  },
+  help: {
+    Notes: 'Specify no action to serve from terminal to view console output',
+  },
+  exec: async command => {
+
+    // status
+    if (command.args.action == 'status') {
+      console.log(`${smolConfig.appName} ${smol.coreName} core is configured to run at ${coreConfig.url}:${coreConfig.port}`)
+      return command.run('pm2 status')
+    }
+
+    // create ecosystem file
+    let serverPath = `${__dirname}/../script/server.js`
+    let ecosystemPath = `${process.cwd()}/config/${smol.coreName}-ecosystem.config.js`
+    let instances = command.args.action ? coreConfig.serverInstances : 1
+    let ecosystem = {
+      apps: [{
+        name: `${smol.string.kabobCase(smolConfig.appName.toLowerCase())}-${smol.coreName}`,
+        script: serverPath,
+        exec_mode: 'cluster',
+        instances,
+        watch: [`${process.cwd()}/core/${smol.coreName}`, `${process.cwd()}/config`],
+        watch_delay: 1000,
+      }],
+    }
+    command.run(`mkdir -p ${process.cwd()}/config`)
+    fs.writeFileSync(ecosystemPath, `module.exports = ${JSON.stringify(ecosystem, null, 2)}`)
+
+    // normal serve
+    if (!command.args.action) command.run(`pm2 start ${ecosystemPath} --no-daemon`)
+
+    // start
+    if (command.args.action == 'start') command.run(`pm2 start ${ecosystemPath}; pm2 save --force`)
+
+    // restart
+    if (command.args.action == 'restart') command.run(`pm2 reload ${ecosystemPath}`)
+
+    // stop
+    if (command.args.action == 'stop') command.run(`pm2 stop ${ecosystemPath}; pm2 delete ${ecosystemPath}; pm2 save --force`)
+
+  }
+}
---
+++ doc/Commands/deploy.md
@@ -0,0 +1,5 @@
+# deploy Command
+
+Once the server is running by using `smol <coreName> serve start`, run `smol <coreName> deploy` to access the core in your browser (browser preference is configured in `config/smol.json`.
+
+It's more convenient to use a tool such as Postman for api testing.
